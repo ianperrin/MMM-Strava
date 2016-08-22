8891e0ca62bef57daa5c092e273ccc755cb6d9c5
ianperrin <ianperrin@gmail.com>
2016-08-22 16:21:31 +0100

    Fixing nodeunit "config is not defined" error

--- test/MMM-Strava.js
+++ test/MMM-Strava.js
@@ -2,11 +2,22 @@
 
 var moduleObject;
 
+// Create stubs for MM global variables
 global.Module = {
 	register: function (name, moduleObjectArgument) {
 		moduleObject = moduleObjectArgument;
 	}
 };
+
+global.config = {
+	units: 'imperial'
+}
+
+global.Log = {
+	info: function (message) {
+		
+	}
+}
 
 var strava = require('../MMM-Strava.js');
 
